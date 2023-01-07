c58ee2a689c83763a09ac0732a739ae214870071
Tharinda Nimnajith Rajapaksha <tharindarajapakshe@gmail.com>
2023-01-08 00:34:08 +0530

    init

--- src/test/groovy/com/example/seleniumgroovygradletestng/MainPageTest.groovy
+++ src/test/groovy/com/example/seleniumgroovygradletestng/MainPageTest.groovy
@@ -4,12 +4,15 @@ import com.codeborne.selenide.Configuration
 import com.codeborne.selenide.Selenide
 import com.codeborne.selenide.logevents.SelenideLogger
 import io.qameta.allure.selenide.AllureSelenide
-import org.testng.annotations.*
-import static org.testng.Assert.*
+import org.testng.annotations.BeforeClass
+import org.testng.annotations.BeforeMethod
+import org.testng.annotations.Test
 
 import static com.codeborne.selenide.Condition.attribute
 import static com.codeborne.selenide.Condition.visible
-import static com.codeborne.selenide.Selenide.*
+import static com.codeborne.selenide.Selenide.$
+import static com.codeborne.selenide.Selenide.open
+import static org.testng.Assert.assertEquals
 
 class MainPageTest {
     MainPage mainPage = new MainPage()
